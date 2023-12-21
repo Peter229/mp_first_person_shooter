112a32f3ac2310b578256c0bb4a82217121960c9
Peter229 <52826658+Peter229@users.noreply.github.com>
2023-12-21 16:30:10 +0000

    First commit

---
+++ src/main.rs
@@ -0,0 +1,53 @@
+use winit::{
+    event::*,
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+
+mod render_state;
+
+fn main() {
+    env_logger::init();
+    
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    let mut render_state = pollster::block_on(render_state::RenderState::new(window));
+
+    event_loop.run(move |event, _, control_flow| match event {
+        Event::WindowEvent {
+            ref event,
+            window_id,
+        } if window_id == render_state.window().id() => match event {
+            WindowEvent::CloseRequested
+            | WindowEvent::KeyboardInput {
+                input:
+                    KeyboardInput {
+                        state: ElementState::Pressed,
+                        virtual_keycode: Some(VirtualKeyCode::Escape),
+                        ..
+                    },
+                ..
+            } => *control_flow = ControlFlow::Exit,
+            WindowEvent::Resized(physical_size) => { 
+                render_state.resize(*physical_size); 
+            }
+            WindowEvent::ScaleFactorChanged { new_inner_size, .. } => { 
+                render_state.resize(**new_inner_size); 
+            }
+            _ => {}
+        },
+        Event::RedrawRequested(window_id) if window_id == render_state.window().id() => {
+            match render_state.render() {
+                Ok(_) => {}
+                Err(wgpu::SurfaceError::Lost) => render_state.resize(render_state.get_size()),
+                Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+                Err(e) => eprintln!("{:?}", e),
+            }
+        }
+        Event::MainEventsCleared => {
+            render_state.window().request_redraw();
+        }
+        _ => {}
+    });
+}
---
+++ src/static_mesh_shader.wgsl
@@ -0,0 +1,24 @@
+struct VertexInput {
+    @location(0) position: vec3<f32>,
+    @location(1) colour: vec3<f32>,
+};
+
+struct VertexOutput {
+    @builtin(position) clip_position: vec4<f32>,
+    @location(0) colour: vec3<f32>,
+};
+
+@vertex
+fn vs_main(
+    vertex: VertexInput,
+) -> VertexOutput {
+    var out: VertexOutput;
+    out.clip_position = vec4<f32>(vertex.position, 1.0);
+    out.colour = vertex.colour;
+    return out;
+}
+
+@fragment
+fn fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
+    return vec4<f32>(in.colour, 1.0);
+}
